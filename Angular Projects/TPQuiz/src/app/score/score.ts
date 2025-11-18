c1f893c048f4c530d76b7313d3293bfb1889d9ad
Emna Kallel <emnakallel10@gmail.com>
2025-11-18 23:06:55 +0100

    Add files via upload

---
+++ Angular Projects/TPQuiz/src/app/score/score.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-score',
+  imports: [RouterLink],
+  templateUrl: './score.html',
+  styleUrl: './score.css',
+})
+export class Score {
+
+}
