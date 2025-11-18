07b98a6265c30eb18f3b76b9c360feb4db69a6c3
Emna Kallel <emnakallel10@gmail.com>
2025-11-19 00:40:20 +0100

    Add files via upload

---
+++ Angular Projects/TP2 profil/src/app/profil/profil.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-profil',
+  imports: [FormsModule],
+  templateUrl: './profil.html',
+  styleUrl: './profil.css',
+})
+export class Profil {
+    utilisateur={
+      prenom:"Emna",
+      age:21,
+    };
+}
